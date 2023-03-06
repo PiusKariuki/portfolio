0059c45f92440e1e5805f99660b7556e9a93d284
PiusKariuki <piuskariuki19@gmail.com>
2023-03-06 16:42:41 +0300

    Feature: about section

--- src/modules/Portfolio/components/Hero/HeroSection.vue
+++ src/modules/Portfolio/components/Hero/HeroSection.vue
@@ -2,7 +2,7 @@
   <div class="content-container flex flex-col pt-32 md:pt-0 gap-2">
     <p class="text-green sm">Hi, my name is</p>
     <p class="text-lightest-slate name font-semibold leading-[1.1]">Pius.</p>
-    <p class="text-slate by-line font-light leading-snug">I build things for the web.</p>
+    <p class="text-slate by-line font-light leading-none my-3">I build things for the web.</p>
     <p class="text-slate md mt-3">
       I’m a software engineer specializing in building (and occasionally designing) exceptional digital
       experiences. Currently, I’m focused on building accessible, human-centered products at
