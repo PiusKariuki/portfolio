d089d8d1e5689fea6611b179c7b911addc4113f0
PiusKariuki <piuskariuki19@gmail.com>
2023-03-08 01:44:47 +0300

    Refactor:about max-w

--- src/modules/Portfolio/sections/AboutMe.vue
+++ src/modules/Portfolio/sections/AboutMe.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="content-container flex flex-col gap-6 mt-16">
-    <heading number="01" text="About Me" />
-    <p class="">
+  <div class="content-container flex flex-col gap-6 mt-16 md:items-center">
+    <heading number="01" text="About Me" class="max-w-5xl" />
+    <p class="max-w-5xl">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
