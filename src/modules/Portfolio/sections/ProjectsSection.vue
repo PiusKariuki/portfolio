d51580f5e69b2f742691050419b2c483ed93ead9
PiusKariuki <piuskariuki19@gmail.com>
2023-03-09 00:35:00 +0300

    Fix: desktop project card

--- src/modules/Portfolio/sections/ProjectsSection.vue
+++ src/modules/Portfolio/sections/ProjectsSection.vue
@@ -2,7 +2,10 @@
   <div class="content-container flex flex-col gap-6 mt-16 md:items-center">
     <heading number="03" text="Some Things I've Built" />
     <div class="flex flex-col w-full gap-8">
-      <mobile-project-card />
+      <mobile-project-card class="md:hidden" />
+      <desktop-project-card :is-even="true" class="hidden md:flex" />
+      <desktop-project-card :is-even="false" class="hidden md:flex" />
+      <desktop-project-card :is-even="true" class="hidden md:flex" />
     </div>
   </div>
 </template>
@@ -10,4 +13,5 @@
 <script setup>
 import Heading from "@/shared/components/Heading.vue";
 import MobileProjectCard from "@/modules/Portfolio/components/MobileProjectCard.vue";
+import DesktopProjectCard from "@/modules/Portfolio/components/DesktopProjectCard.vue";
 </script>
