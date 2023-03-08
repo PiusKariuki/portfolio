fd2b696aa3e9f55b0aa3491db5e59d6a19a2d2b9
PiusKariuki <piuskariuki19@gmail.com>
2023-03-08 23:58:01 +0300

    Fix: mobile project card

---
+++ src/modules/Portfolio/components/MobileProjectCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full px-4 py-8 flex flex-col text-white shadow-2xl bg-green-tint relative rounded-md">
+    <img
+        alt=""
+        class="absolute inset-0 w-full h-full opacity-10" src="/imgs/screenshots/speeddates.png">
+    <div class="flex flex-col z-20">
+      <p class="xs text-green">Featured Project</p>
+      <p class="xxl font-bold tracking-wide text-white hover:text-green cursor-pointer">Spotify profile</p>
+      <p class="lg text-white mt-3">
+        A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks,
+        and detailed audio information about each track. Create and save new playlists of recommended tracks based on
+        your
+        existing playlists and more.
+      </p>
+      <p class="lg text-white mt-8">React Express NodeJS</p>
+
+      <svg
+          class="h-6 w-6 mt-6 cursor-pointer"
+          aria-hidden="true" fill="none" stroke="currentColor" stroke-width="2.0" viewBox="0 0 24 24"
+           xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"></path>
+      </svg>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
