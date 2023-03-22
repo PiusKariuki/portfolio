e2889b2ae4b9af811eb9c77ef1e76b0aeba29813
PiusKariuki <piuskariuki19@gmail.com>
2023-03-22 15:30:11 +0300

    Feature: rounded outside corners on companies

--- src/modules/Portfolio/sections/Companies.vue
+++ src/modules/Portfolio/sections/Companies.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="content-container text-lightest-slate ">
-    <div class="flex flex-col md:flex-row shadow-lg w-full rounded-lg bg-light-navy  p-4 lg:pr-20 md:py-16 gap-3  md:gap-12 md:justify-between">
+    <div
+        class="flex flex-col md:flex-row shadow-lg w-full rounded-lg bg-light-navy  p-4 lg:px-12 md:py-16 gap-3
+        md:gap-12 md:justify-between">
       <div class="flex flex-col gap-6 max-w-sm">
         <p class="text-green h2 mb-3">
           Organizations I have worked with.
@@ -16,7 +18,9 @@
             target="_blank"
             :key="item.url"
             v-for="(item, index) in companies"
-            class="w-full h-24 md:h-28 lg:h-28 bg-lightest-navy flex justify-center items-center">
+            class="w-full h-24 md:h-28 lg:h-28 bg-lightest-navy flex justify-center items-center"
+            :class="item.containerStyles"
+        >
           <img
               :class="item.styles"
               :key="index"
@@ -33,21 +37,25 @@ const companies = [
   {
     logo: "/imgs/companies/medrite.svg",
     styles: "w-24 lg:w-32",
+    containerStyles: "rounded-tl-md",
     url: "https://medriteurgentcare.com/"
   },
   {
     logo: "/imgs/companies/welz.svg",
     styles: "w-24 lg:w-32",
+    containerStyles: "rounded-tr-md",
     url: "https://www.welz.com/"
   },
   {
     logo: "/imgs/companies/xeron.svg",
     styles: "w-20 lg:w-24",
-    url: "https://www.xeronlabs.com/"
+    containerStyles: "rounded-bl-md",
+    // url: "https://www.xeronlabs.com/"
   },
   {
     logo: "/imgs/companies/speeddates.svg",
     styles: "h-10 lg:h-14 lg:w-32",
+    containerStyles: "rounded-br-md",
     url: "https://www.speeddateskenya.com/"
   },
 ]
