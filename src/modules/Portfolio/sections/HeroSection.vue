c04d56fd0baebf749374a94c97f38625e478d83c
PiusKariuki <piuskariuki19@gmail.com>
2023-05-17 21:03:18 +0300

    Feature: hero wording

--- src/modules/Portfolio/sections/HeroSection.vue
+++ src/modules/Portfolio/sections/HeroSection.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class=" flex flex-col pt-32 md:pt-0 gap-2 w-full min-h-full">
-    <p class="text-green paragraph">Hi, my name is</p>
-    <p class="text-lightest-slate name font-semibold leading-[1.1]">Pius Kariuki.</p>
-    <p class="text-slate h2 my-6 w-full">I am a software engineer.</p>
-    <p class="text-lightest-slate paragraph  mt-3 lg:w-10/12">
-      I’m currently building and maintaining accessible
-      healthcare platforms at
-      <span class="text-green">Welz.</span>
-    </p>
-    <a
-        :href="emailAddress"
-        class="cta mt-12 lg:mt-auto justify-self-end py-[0.75rem] w-44 flex items-center justify-center">Reach out!</a>
-  </div>
+    <div class=" flex flex-col pt-32 md:pt-0 gap-2 w-full min-h-full">
+        <p class="text-lightest-slate name font-semibold leading-[1.1]">
+            <span class="text-green">Hello, </span>I am Pius
+            Kariuki</p>
+        <p class="text-lightest-slate h2 my-6 w-full">Full stack web developer.</p>
+        <p class="text-lightest-slate paragraph  mt-3 lg:w-10/12">
+            A solution-focused fullstack developer. I specialize in building scalable and maintainable software.
+        </p>
+
+        <p class="text-lightest-slate paragraph  mt-3 lg:w-10/12">Striving to make the web a beautiful place one line of
+            code at a time !</p>
+        <a
+                :href="emailAddress"
+                class="cta mt-12 lg:mt-auto justify-self-end py-[0.75rem] w-44 flex items-center justify-center">
+           Let's get in touch
+        </a>
+    </div>
 </template>
 
 <script setup>
@@ -19,11 +23,8 @@ import {emailAddress} from "@/shared/data/email.js";
 </script>
 
 <style>
-.name{
-  font-size: clamp(40px, 8vw, 80px);
+.name {
+    font-size: clamp(40px, 8vw, 80px);
 }
 
-.by-line{
-  font-size: clamp(40px, 8vw, 80px);
-}
 </style>
